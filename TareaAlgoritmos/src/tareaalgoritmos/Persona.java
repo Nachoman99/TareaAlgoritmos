fae3ad0070bd0b37418407b228d860c7148fdcd9
Nacho <Nacho@192.168.0.16>
2019-06-11 19:55:20 -0600

    creacion de persona

---
+++ TareaAlgoritmos/src/tareaalgoritmos/Persona.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tareaalgoritmos;
+
+/**
+ *
+ * @author Nacho
+ */
+public class Persona {
+    private int cedula;
+    private String nombre;
+    private int edad;
+
+    public Persona() {
+    }
+
+    public Persona(int cedula, String nombre, int edad) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public int getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "cedula=" + cedula + ", nombre=" + nombre + ", edad=" + edad + '}';
+    }
+    
+}
